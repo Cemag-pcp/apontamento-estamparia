191884b6fab458d1b8277bba092b6f1bde291cd8
luan <luan640@gmail.com>
2023-05-29 13:10:42 -0300

    abas operadores

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 # Função para obter os dados da planilha
 
 # Rota inicial da aplicação
-@app.route('/')
-def index():
+@app.route('/4238')
+def operador_4238():
 
     def get_sheet_data_4238():
 
@@ -48,11 +48,11 @@ def index():
     sheet_data, table1 = get_sheet_data_4238()
     print(table1)
 
-    return render_template('index.html', sheet_data=sheet_data)
+    return render_template('operador_4238.html', sheet_data=sheet_data)
 
 # Rota para enviar a linha para outra planilha
-@app.route('/send_row', methods=['POST'])
-def send_row():
+@app.route('/send_row_4238', methods=['POST'])
+def send_row_4238():
 
     def att_linha(id, qtReal, maquina, qtMortas, finalizou):
 
@@ -106,6 +106,301 @@ def send_row():
 
         #retorna mensagem de sucesso
         return jsonify({'mensagem': 'Dados enviados com sucesso'})
+
+# Rota inicial da aplicação
+@app.route('/4217')
+def operador_4217():
+
+    def get_sheet_data_4217():
+
+        scope = ['https://www.googleapis.com/auth/spreadsheets',
+                "https://www.googleapis.com/auth/drive"]
+        
+        credentials = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+        client = gspread.authorize(credentials)
+        sa = gspread.service_account('service_account.json')    
+
+        name_sheet1 = 'RQ PCP-003-001 (APONTAMENTO ESTAMPARIA) / RQ PCP-009-000 (SEQUENCIAMENTO ESTAMPARIA) e RQ CQ-015-000 (Inspeção da Estamparia)'
+        worksheet1 = 'OPERADOR 4217'
+        sh1 = sa.open(name_sheet1)
+        wks1 = sh1.worksheet(worksheet1)
+        list1 = wks1.get()
+
+        header = wks1.row_values(5)
+
+        table1 = pd.DataFrame(list1)     
+        table1 = table1.set_axis(header, axis=1)
+
+        table1 = table1.iloc[5:]
+        
+        table1.reset_index(drop=True)
+
+        table1 = table1.loc[(table1['CÓDIGO'] != '') & (table1['MÁQUINA'] == '')]
+
+        table1 = table1[(table1['Finalizou?'] == 'Não') | (table1['Finalizou?'] == '')]
+
+        values = table1.values.tolist()
+        
+        return values, table1
+
+    sheet_data, table1 = get_sheet_data_4217()
+    print(table1)
+
+    return render_template('operador_4217.html', sheet_data=sheet_data)
+
+# Rota para enviar a linha para outra planilha
+@app.route('/send_row_4217', methods=['POST'])
+def send_row_4217():
+
+    def att_linha(id, qtReal, maquina, qtMortas, finalizou):
+
+        scope = ['https://www.googleapis.com/auth/spreadsheets',
+                "https://www.googleapis.com/auth/drive"]
+        
+        credentials = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+        client = gspread.authorize(credentials)
+        sa = gspread.service_account('service_account.json')    
+
+        name_sheet1 = 'RQ PCP-003-001 (APONTAMENTO ESTAMPARIA) / RQ PCP-009-000 (SEQUENCIAMENTO ESTAMPARIA) e RQ CQ-015-000 (Inspeção da Estamparia)'
+        worksheet1 = 'OPERADOR 4217'
+        sh1 = sa.open(name_sheet1)
+        wks1 = sh1.worksheet(worksheet1)
+        list1 = wks1.get()
+
+        header = wks1.row_values(5)
+
+        table1 = pd.DataFrame(list1)     
+        table1 = table1.set_axis(header, axis=1)
+
+        table1 = table1.iloc[5:]
+        
+        table1.reset_index(drop=True)
+
+        table1 = table1[table1['ID'] == id]
+
+        linha_planilha = table1.index[0]
+
+        wks1.update("F" + str(linha_planilha + 1), qtReal) # qt real
+        wks1.update("I" + str(linha_planilha + 1), qtMortas) # qt morta
+        wks1.update("G" + str(linha_planilha + 1), maquina) # maquina
+        wks1.update("M" + str(linha_planilha + 1), finalizou) # finalizou
+
+    if request.method == 'POST':
+
+        linha = request.get_json()  # Obter os dados da linha enviados pelo front-end
+
+        id_linha = linha[0]
+        qtReal = linha[5]
+        maquina = linha[7]
+        qtMortas = linha[6]
+        finalizou = linha[8]
+
+        print(id_linha, qtReal, maquina, qtReal, finalizou)
+
+        att_linha(id_linha, qtReal, maquina, qtMortas, finalizou)
+        print("ok, atualizou")
+        
+        #sheet_data, table1 = get_sheet_data_4238()
+
+        #retorna mensagem de sucesso
+        return jsonify({'mensagem': 'Dados enviados com sucesso'})
+
+# Rota inicial da aplicação
+@app.route('/3654')
+def operador_3654():
+
+    def get_sheet_data_3654():
+
+        scope = ['https://www.googleapis.com/auth/spreadsheets',
+                "https://www.googleapis.com/auth/drive"]
+        
+        credentials = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+        client = gspread.authorize(credentials)
+        sa = gspread.service_account('service_account.json')    
+
+        name_sheet1 = 'RQ PCP-003-001 (APONTAMENTO ESTAMPARIA) / RQ PCP-009-000 (SEQUENCIAMENTO ESTAMPARIA) e RQ CQ-015-000 (Inspeção da Estamparia)'
+        worksheet1 = 'OPERADOR 3654'
+        sh1 = sa.open(name_sheet1)
+        wks1 = sh1.worksheet(worksheet1)
+        list1 = wks1.get()
+
+        header = wks1.row_values(5)
+
+        table1 = pd.DataFrame(list1)     
+        table1 = table1.set_axis(header, axis=1)
+
+        table1 = table1.iloc[5:]
+        
+        table1.reset_index(drop=True)
+
+        table1 = table1.loc[(table1['CÓDIGO'] != '') & (table1['MÁQUINA'] == '')]
+
+        table1 = table1[(table1['Finalizou?'] == 'Não') | (table1['Finalizou?'] == '')]
+
+        values = table1.values.tolist()
+        
+        return values, table1
+
+    sheet_data, table1 = get_sheet_data_3654()
+    print(table1)
+
+    return render_template('operador_3654.html', sheet_data=sheet_data)
+
+# Rota para enviar a linha para outra planilha
+@app.route('/send_row_4217', methods=['POST'])
+def send_row_3654():
+
+    def att_linha(id, qtReal, maquina, qtMortas, finalizou):
+
+        scope = ['https://www.googleapis.com/auth/spreadsheets',
+                "https://www.googleapis.com/auth/drive"]
+        
+        credentials = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+        client = gspread.authorize(credentials)
+        sa = gspread.service_account('service_account.json')    
+
+        name_sheet1 = 'RQ PCP-003-001 (APONTAMENTO ESTAMPARIA) / RQ PCP-009-000 (SEQUENCIAMENTO ESTAMPARIA) e RQ CQ-015-000 (Inspeção da Estamparia)'
+        worksheet1 = 'OPERADOR 4217'
+        sh1 = sa.open(name_sheet1)
+        wks1 = sh1.worksheet(worksheet1)
+        list1 = wks1.get()
+
+        header = wks1.row_values(5)
+
+        table1 = pd.DataFrame(list1)     
+        table1 = table1.set_axis(header, axis=1)
+
+        table1 = table1.iloc[5:]
+        
+        table1.reset_index(drop=True)
+
+        table1 = table1[table1['ID'] == id]
+
+        linha_planilha = table1.index[0]
+
+        wks1.update("F" + str(linha_planilha + 1), qtReal) # qt real
+        wks1.update("I" + str(linha_planilha + 1), qtMortas) # qt morta
+        wks1.update("G" + str(linha_planilha + 1), maquina) # maquina
+        wks1.update("M" + str(linha_planilha + 1), finalizou) # finalizou
+
+    if request.method == 'POST':
+
+        linha = request.get_json()  # Obter os dados da linha enviados pelo front-end
+
+        id_linha = linha[0]
+        qtReal = linha[5]
+        maquina = linha[7]
+        qtMortas = linha[6]
+        finalizou = linha[8]
+
+        print(id_linha, qtReal, maquina, qtReal, finalizou)
+
+        att_linha(id_linha, qtReal, maquina, qtMortas, finalizou)
+        print("ok, atualizou")
+        
+        #sheet_data, table1 = get_sheet_data_4238()
+
+        #retorna mensagem de sucesso
+        return jsonify({'mensagem': 'Dados enviados com sucesso'})
+
+# Rota inicial da aplicação
+@app.route('/4200')
+def operador_4200():
+
+    def get_sheet_data_4200():
+
+        scope = ['https://www.googleapis.com/auth/spreadsheets',
+                "https://www.googleapis.com/auth/drive"]
+        
+        credentials = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+        client = gspread.authorize(credentials)
+        sa = gspread.service_account('service_account.json')    
+
+        name_sheet1 = 'RQ PCP-003-001 (APONTAMENTO ESTAMPARIA) / RQ PCP-009-000 (SEQUENCIAMENTO ESTAMPARIA) e RQ CQ-015-000 (Inspeção da Estamparia)'
+        worksheet1 = 'OPERADOR 4200'
+        sh1 = sa.open(name_sheet1)
+        wks1 = sh1.worksheet(worksheet1)
+        list1 = wks1.get()
+
+        header = wks1.row_values(5)
+
+        table1 = pd.DataFrame(list1)     
+        table1 = table1.iloc[:,:14].set_axis(header, axis=1)
+
+        table1 = table1.iloc[5:]
+        
+        table1.reset_index(drop=True)
+
+        table1 = table1.loc[(table1['CÓDIGO'] != '') & (table1['MÁQUINA'] == '')]
+
+        table1 = table1[(table1['Finalizou?'] == 'Não') | (table1['Finalizou?'] == '')]
+
+        values = table1.values.tolist()
+        
+        return values, table1
+
+    sheet_data, table1 = get_sheet_data_4200()
+    print(table1)
+
+    return render_template('operador_4200.html', sheet_data=sheet_data)
+
+# Rota para enviar a linha para outra planilha
+@app.route('/send_row_4200', methods=['POST'])
+def send_row_4200():
+
+    def att_linha(id, qtReal, maquina, qtMortas, finalizou):
+
+        scope = ['https://www.googleapis.com/auth/spreadsheets',
+                "https://www.googleapis.com/auth/drive"]
+        
+        credentials = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+        client = gspread.authorize(credentials)
+        sa = gspread.service_account('service_account.json')    
+
+        name_sheet1 = 'RQ PCP-003-001 (APONTAMENTO ESTAMPARIA) / RQ PCP-009-000 (SEQUENCIAMENTO ESTAMPARIA) e RQ CQ-015-000 (Inspeção da Estamparia)'
+        worksheet1 = 'OPERADOR 4200'
+        sh1 = sa.open(name_sheet1)
+        wks1 = sh1.worksheet(worksheet1)
+        list1 = wks1.get()
+
+        header = wks1.row_values(5)
+
+        table1 = pd.DataFrame(list1)     
+        table1 = table1.iloc[:,:14].set_axis(header, axis=1)
+
+        table1 = table1.iloc[5:]
+        
+        table1.reset_index(drop=True)
+
+        table1 = table1[table1['ID'] == id]
+
+        linha_planilha = table1.index[0]
+
+        wks1.update("F" + str(linha_planilha + 1), qtReal) # qt real
+        wks1.update("I" + str(linha_planilha + 1), qtMortas) # qt morta
+        wks1.update("G" + str(linha_planilha + 1), maquina) # maquina
+        wks1.update("M" + str(linha_planilha + 1), finalizou) # finalizou
+
+    if request.method == 'POST':
+
+        linha = request.get_json()  # Obter os dados da linha enviados pelo front-end
+
+        id_linha = linha[0]
+        qtReal = linha[5]
+        maquina = linha[7]
+        qtMortas = linha[6]
+        finalizou = linha[8]
+
+        print(id_linha, qtReal, maquina, qtReal, finalizou)
+
+        att_linha(id_linha, qtReal, maquina, qtMortas, finalizou)
+        print("ok, atualizou")
+        
+        #sheet_data, table1 = get_sheet_data_4238()
+
+        #retorna mensagem de sucesso
+        return jsonify({'mensagem': 'Dados enviados com sucesso'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
